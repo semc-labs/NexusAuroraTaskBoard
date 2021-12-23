6446e72eee9e39dd720e8e1a41e173b4108b5ba0
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2021-12-22 18:59:37 -0600

    Fix Build. (node-fetch updated)

--- buttons/acceptTweet.js
+++ buttons/acceptTweet.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 module.exports.func = async function(interaction) {
 	fetch(process.env.LINK+"/tweet/", {
         method: "post",
--- commands/soonestFlight.js
+++ commands/soonestFlight.js
@@ -1,6 +1,6 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const fs = require('fs');
 
 function formatNums(num, sep, dec, u) { sep = sep || ','; u = u || '\\d'; if (typeof num != 'string') { num = String(num); if (dec && dec != '.') num = num.replace('.', dec); } return num.replace(RegExp('\\' + (dec || '.') + u + '+|' + u + '(?=(?:' + u + '{3})+(?!' + u + '))', 'g'), function (a) { return a.length == 1 ? a + sep : a }) }
